52a7e99f0c02d2e3b258e50e527c380b821d26d4
Shridhara Chaube <chaubey.shridhara@gmail.com>
2022-12-05 15:02:09 +0530

    shridhara commit

---
+++ SampleRepositry_1/SampleRESTApi_1/src/main/java/com/p1/ControllerMansi.java
@@ -0,0 +1,6 @@
+package com.p1;
+
+public class ControllerMansi {
+
+}
+
---
+++ SampleRepositry_1/SampleRESTApi_1/src/main/java/com/p1/SampleRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApi1Application.class, args);
+	}
+
+}
